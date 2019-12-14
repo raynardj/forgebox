7c500e98f378399ae8b70053b94d664e85066c90
raynardj <raynard@rasenn.com>
2019-12-14 16:08:31 +0800

    restructure

--- initialize.py
+++
@@ -1,9 +0,0 @@
-from forge.config import SQLALCHEMY_DATABASE_URI
-from configparser import ConfigParser
-
-config = ConfigParser()
-
-config["DATABASE"] = {"fg_db":SQLALCHEMY_DATABASE_URI}
-
-with open('/etc/forgebox.cfg', 'w') as configfile:
-    config.write(configfile)
--- utils.py
+++
@@ -1,4 +0,0 @@
-import os
-def create_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
