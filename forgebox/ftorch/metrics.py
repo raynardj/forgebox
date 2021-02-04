80853add7a1bb229f49bd7a1ace85e6edc90bd3d
raynardj <raynard@rasenn.com>
2021-02-04 11:16:44 +0800

    🛀🏾 cleaning up relic code

--- forgebox/ftorch/metrics.py
+++ forgebox/ftorch/metrics.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def argmax(x):
     """
     Arg max of a torch tensor (2 dimensional, dim=1)
@@ -8,7 +9,8 @@ def argmax(x):
     """
     return torch.max(x, dim=1)[1]
 
-def metric4_bi(y_pred,y_true,bm = 0.5):
+
+def metric4_bi(y_pred, y_true, bm=0.5):
     """
         4 metrics for binary classification situations (single output)
         :param y_pred:
@@ -22,7 +24,8 @@ def metric4_bi(y_pred,y_true,bm = 0.5):
     f1_ = f1(rec, prec)
     return acc, rec, prec, f1_
 
-def metric4(y_pred,y_true,bm = 0.5):
+
+def metric4(y_pred, y_true, bm=0.5):
     """
     4 metrics for binary classification situations (multiple output)
     :param y_pred:
@@ -30,14 +33,16 @@ def metric4(y_pred,y_true,bm = 0.5):
     :param bm: benchmark, default 0.5
     :return: acc,rec,prec,f1_score
     """
-    acc = accuracy(y_pred,y_true)
-    rec = recall(y_pred,y_true,bm)
-    prec = precision(y_pred,y_true,bm)
-    f1_ = f1(rec,prec)
-    return acc,rec,prec,f1_
+    acc = accuracy(y_pred, y_true)
+    rec = recall(y_pred, y_true, bm)
+    prec = precision(y_pred, y_true, bm)
+    f1_ = f1(rec, prec)
+    return acc, rec, prec, f1_
+
 
 def accuracy_bi(y_pred, y_true, bm=.5):
-    return ((y_pred>bm)==(y_true>bm)).float().mean()
+    return ((y_pred > bm) == (y_true > bm)).float().mean()
+
 
 def accuracy(y_pred, y_true):
     """
@@ -47,21 +52,24 @@ def accuracy(y_pred, y_true):
     """
     return (argmax(y_pred) == y_true).float().mean()
 
-def recall(y_pred,y_true,bm = 0.5):
-    is_right = (((y_pred > bm)*1).long()==y_true.long())
-    is_right_in_targ = is_right[y_true==1]
+
+def recall(y_pred, y_true, bm=0.5):
+    is_right = (((y_pred > bm)*1).long() == y_true.long())
+    is_right_in_targ = is_right[y_true == 1]
     if len(is_right_in_targ) == 0:
         is_right_in_targ = torch.zeros(1)
     return is_right_in_targ.float().mean()
 
-def precision(y_pred,y_true, bm = 0.5):
-    is_right = (((y_pred > bm)*1).long()==y_true.long())
+
+def precision(y_pred, y_true, bm=0.5):
+    is_right = (((y_pred > bm)*1).long() == y_true.long())
     is_right_in_pred = is_right[(y_pred > bm)]
     if len(is_right_in_pred) == 0:
         is_right_in_pred = torch.zeros(1)
     return is_right_in_pred.float().mean()
 
-def f1(recall,precision):
+
+def f1(recall, precision):
     """
     stantard f1 for binary classification
     recall:
--- forgebox/ftorch/utils.py
+++ forgebox/ftorch/utils.py
@@ -1,21 +1,28 @@
 from forgebox.utils import JUPYTER
-if JUPYTER: from IPython.display import display
+if JUPYTER:
+    from IPython.display import display
 import torch
 import os
+
 
 def mars(printstuff):
     """
     A print function works between jupyter and console print
     :param printstuff:
     """
-    if JUPYTER:display(printstuff)
-    else:print(printstuff)
+    if JUPYTER:
+        display(printstuff)
+    else:
+        print(printstuff)
 
-def save_model(model,path):
+
+def save_model(model, path):
     torch.save(model.state_dict(), path)
 
-def load_model(model,path):
+
+def load_model(model, path):
     model.load_state_dict(torch.load(path))
+
 
 def stty_size():
     try:
